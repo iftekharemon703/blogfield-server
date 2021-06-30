e690be2a3791b0469374129cb28849696e8f69c1
iftekharemon703 <iftekharemon703@gmail.com>
2021-07-01 00:28:59 +0600

    fix some issue

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
-require('dotenv').config();
-
+require('dotenv').config()
 
 const port = 5000;
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e8itm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority";
+const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esrkv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
@@ -17,40 +17,50 @@ app.use(cors());
 
 
 client.connect(err => {
-  const blogCollection = client.db("blogfield").collection("blog");
+  const blogCollection = client.db(process.env.DB_NAME).collection("blog");
   
-  app.post('/addBlog', (req, res) => {
-    const blog = req.body;
-    blogCollection.insertOne(blog)
-        .then(result => {
-            res.send(result.insertedCount > 0);
-        })
-  })
+  if(err){
+    console.log("wrong")
+  }else{
+    console.log('database connected');
 
-  app.get('/blogs', (req, res) => {
-    blogCollection.find({})
-    .toArray((err, blog) => {
-      res.send(blog)
+    app.get('/', (req, res) => {
+      res.send('database connected')
     })
-  })
 
-  app.get('/blog/:id', (req, res) =>{
-    console.log(req.params.id);
-    blogCollection.find({ _id: ObjectId(req.params.id) })
-    .toArray((err, blog) => {
-      res.send(blog[0])
-    })
-  })
-
-  app.delete('/deleteBlog/:id', (req, res) => {
-    blogCollection.deleteOne({ _id: ObjectId(req.params.id) })
-      .then(result => {
-        res.send(result.deletedCount > 0)
+    app.get('/blogs', (req, res) => {
+      blogCollection.find({})
+      .toArray((err, blog) => {
+        res.send(blog)
       })
-  })
-  app.get('/', (req, res) => {
-    res.send('database connected')
-  })
+    })
+
+    app.post('/addBlog', (req, res) => {
+      const blog = req.body;
+      blogCollection.insertOne(blog)
+          .then(result => {
+              res.send(result.insertedCount > 0);
+          })
+    })
+
+    app.get('/blog/:id', (req, res) =>{
+      console.log(req.params.id);
+      blogCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, blog) => {
+        res.send(blog[0])
+      })
+    })
+
+    app.delete('/deleteBlog/:id', (req, res) => {
+      blogCollection.deleteOne({ _id: ObjectId(req.params.id) })
+        .then(result => {
+          res.send(result.deletedCount > 0)
+        })
+    })
+
+  }
+
+
 });
 
 app.listen(process.env.PORT || port);
